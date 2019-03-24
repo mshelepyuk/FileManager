fa5a7ddcacf39568f720079f4429595af80c5a4b
Maxim Shelepyuk <unccynin@gmail.com>
2019-03-24 17:19:42 +0700

    Initial Commit

---
+++ FileManager/FileManager.h
@@ -0,0 +1,19 @@
+//
+//  FileManager.h
+//  FileManager
+//
+//  Created by Maxim Shelepyuk on 24/03/2019.
+//  Copyright © 2019 Maxim Shelepyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FileManager.
+FOUNDATION_EXPORT double FileManagerVersionNumber;
+
+//! Project version string for FileManager.
+FOUNDATION_EXPORT const unsigned char FileManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileManager/PublicHeader.h>
+
+
